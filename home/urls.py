69ebd0ecc62e4760fb617578792e6b3939524d96
FelipeStafoge <felipestafoge11@gmail.com>
2024-01-05 18:04:09 -0300

    Criando começo de templates

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'index'
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('take_a_card/', views.take_a_card, name='take_a_card'),
 ]
